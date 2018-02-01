4b2a54cbd29711e6445cc77ed4c233b52e6549a7
Karabanov Boris <bkarabanov@ics.perm.ru>
2018-02-01 15:57:04 +0500

    add getBalances

---
+++ RaiBlocks/Results/BalancesResult.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using RaiBlocks.Converters;
+using System.Collections.Generic;
+
+namespace RaiBlocks.Results
+{
+    public class BalancesResult
+    {
+        //[JsonConverter(typeof(StringToRawConverter))]
+        [JsonProperty("balances")]
+        public Dictionary<string, BalanceResult> Balances { get; set; }
+    }
+}
